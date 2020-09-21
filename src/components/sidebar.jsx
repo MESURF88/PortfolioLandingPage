be82b6a1973b501c5d8d7837bccb989b31151228
Kevin <hill.kevin@earthlink.net>
2020-09-20 22:58:47 -0400

    Replace with functional site

---
+++ src/components/sidebar.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+
+//import AuthorImg from '../images/fullimg_orig.jpg'
+
+
+export default class Sidebar extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <nav href="#navbar" className="js-colorlib-nav-toggle colorlib-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i /></nav>
+          <aside id="colorlib-aside" className="border js-fullheight">
+            <div className="text-center">
+
+              <div className="author-img" style={{backgroundImage: 'url(images/fullimg_orig.jpg)'}} />
+              <h1 id="colorlib-logo"><a href="index.html">Kevin Hill</a></h1>
+              <span className="email"><i className="icon-mail"></i> hill.kevin2222@gmail.com</span>
+            </div>
+            <nav id="colorlib-main-menu" role="navigation" className="navbar">
+              <div id="navbar" className="collapse">
+                <ul>
+                  <li className="active"><a href="#home" data-nav-section="home">Introduction</a></li>
+                  <li><a href="#about" data-nav-section="about">About</a></li>
+                  <li><a href="#projects" data-nav-section="projects">Projects</a></li>
+                  <li><a href="#timeline" data-nav-section="timeline">Timeline</a></li>
+                </ul>
+              </div>
+            </nav>
+            <nav id="colorlib-main-menu">
+              <ul>
+                <li><a href="https://www.linkedin.com/in/kevinhill88/" target="_blank" rel="noopener noreferrer"><i className="icon-linkedin2" /></a></li>
+                <li><a href="https://github.com/MESURF88" target="_blank" rel="noopener noreferrer"><i className="icon-github"></i></a></li>
+              </ul>
+            </nav>
+            <div className="colorlib-footer">
+              <p><small>
+                  Made with <i className="icon-coffee" aria-hidden="true" /> <br></br>
+                  Thanks <a href="https://colorlib.com" target="_blank" rel="noopener noreferrer">Colorlib</a> for inspiration 
+              </small></p>
+
+            </div>
+          </aside>
+        </div>
+      </div>
+    )
+  }
+}
