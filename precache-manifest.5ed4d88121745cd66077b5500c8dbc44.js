bc3ccf4d3462490ae438da7950e43ba4064ee4da
Kevin <hill.kevin@earthlink.net>
2020-09-21 18:08:49 -0400

    Updates

---
+++ precache-manifest.5ed4d88121745cd66077b5500c8dbc44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "96e65846d65153e356ca",
+    "url": "/me/static/js/main.96e65846.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "96e65846d65153e356ca",
+    "url": "/me/static/css/main.d5a2fdf6.chunk.css"
+  },
+  {
+    "revision": "2a17fae4843e9bf4b57d319991e8af15",
+    "url": "/me/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/PortfolioLandingPage/precache-manifest.5a582bdd42bb1aff47903c2bbd79ffc2.js"
+  "/me/precache-manifest.5ed4d88121745cd66077b5500c8dbc44.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +25,10 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/PortfolioLandingPage/index.html"), {
+workbox.routing.registerNavigationRoute("/me/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
