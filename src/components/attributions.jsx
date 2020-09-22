5afa2b7540c3fccf938176753ecb6b33e3165beb
Kevin <hill.kevin@earthlink.net>
2020-09-21 23:16:27 -0400

    Added react logo to bottom of page.

--- src/components/attributions.jsx
+++ src/components/attributions.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import React_Logo from '../snippets/react.png';
 
 export default class Attributions extends Component {
   render() {
@@ -9,6 +10,9 @@ export default class Attributions extends Component {
               <p>Attribution:</p>
               <small>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
               <br></br>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a></small>
+            </div>
+            <div className="project">
+              Made with React <img  style={{width: '5%', height: '15%'}} src={React_Logo} alt="react_icon"/> 
             </div>
         </div>
       </div>
